7431289c002ff0777b1d54d1fa5ebba9eeb0bfe4
shuntawatanabe0123 <resuta.0123@gmail.com>
2022-09-20 00:51:49 +0900

    iosとandroidのどちらも表示できている

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(const MyApp());
@@ -23,18 +26,49 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
+  Position? currentPosition;
   late GoogleMapController _controller;
-//初期位置
+  late StreamSubscription<Position> positionStream;
+  //初期位置
   final CameraPosition _kGooglePlex = const CameraPosition(
-    target: LatLng(35.6695, 139.7590),
+    target: LatLng(35.6664, 139.7583),
     zoom: 18,
   );
+
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
+    distanceFilter: 100,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    //位置情報が許可されていない時に許可をリクエストする
+    Future(() async {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if(permission == LocationPermission.denied){
+        await Geolocator.requestPermission();
+      }
+    });
+
+    //現在位置を更新し続ける
+    positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+      currentPosition = position;
+      print(position == null
+          ? 'Unknown'
+          : '${position.latitude.toString()}, ${position.longitude.toString()}');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
       mapType: MapType.normal,
       initialCameraPosition: _kGooglePlex,
+      myLocationEnabled: true,//現在位置をマップ上に表示
       onMapCreated: (GoogleMapController controller) {
         _controller = controller;
       },
