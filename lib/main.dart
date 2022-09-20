124be2e4dd7976c7608642535ff36b63feabd52d
shuntawatanabe0123 <resuta.0123@gmail.com>
2022-09-20 17:53:35 +0900

    “押したらダイアログする”

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 void main() => runApp(const MyApp());
 
@@ -31,7 +33,7 @@ class MapSampleState extends State<MapSample> {
   late StreamSubscription<Position> positionStream;
   //初期位置
   final CameraPosition _kGooglePlex = const CameraPosition(
-    target: LatLng(35.6664, 139.7583),
+    target: LatLng(35.6690, 139.7588),
     zoom: 18,
   );
 
@@ -39,7 +41,7 @@ class MapSampleState extends State<MapSample> {
     accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
     distanceFilter: 100,
   );
-
+  
   @override
   void initState() {
     super.initState();
@@ -62,16 +64,70 @@ class MapSampleState extends State<MapSample> {
           : '${position.latitude.toString()}, ${position.longitude.toString()}');
     });
   }
+  
+
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
       mapType: MapType.normal,
       initialCameraPosition: _kGooglePlex,
+      markers: _createMarker(),
       myLocationEnabled: true,//現在位置をマップ上に表示
       onMapCreated: (GoogleMapController controller) {
         _controller = controller;
       },
+
     );
   }
+Set<Marker> _createMarker() {
+ return {
+   Marker(
+     markerId: MarkerId("marker_1"),
+     position: LatLng(35.6700, 139.7592),
+     infoWindow: InfoWindow(title: "俺のやきとり", snippet: 'https://www.oreno.co.jp/restaurant/yakitori_ginza9'),
+     onTap: () {
+         showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('Aの動作の確認'),
+                    content: Text("This is the content"),
+                    actions: [
+                      TextButton(
+                        child: Text("Cancel"),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      TextButton(
+                      child: Text("OK"),
+                      onPressed: () => launchUrl(),
+                    ),
+                  ],
+                  );
+                });
+     },
+     ),
+    Marker(
+      markerId: MarkerId("marker_2"),
+      position: LatLng(35.6710, 139.7596),
+      // infoWindow: InfoWindow(title: "天ぷらとワイン"),
+      onTap: () {
+        print("Clicked"); 
+      },
+    ),
+ };
 }
+
+   Future<void> launchUrl() async {
+    final url = "https://www.oreno.co.jp/restaurant/yakitori_ginza9";
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      final Error error = ArgumentError('Error launching $url');
+      throw error;
+    }
+  }
+
+  
+}
+
